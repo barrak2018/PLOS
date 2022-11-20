40c9fc3c3edca4fafe3873b9e62552057697cf25
Barrack <41580946+barrak2018@users.noreply.github.com>
2022-11-20 11:19:48 -0400

    resolver errores de bootstrap local en proceso

--- includes/header.php
+++ includes/header.php
@@ -5,10 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de control de Libros</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
-
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
     <nav class = "navbar navbar-dark bg-dark">
@@ -18,3 +15,5 @@
         </div>
     </nav>
     <?php $year = date("Y"); ?>
+
+
